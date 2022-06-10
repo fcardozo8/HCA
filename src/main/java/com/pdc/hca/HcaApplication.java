ad0c30ad4f254ecd238b154de087658532f91aad
Franco <franco.racing.cardozo@gmail.com>
2022-06-10 15:07:50 -0300

    Initial commit

---
+++ src/main/java/com/pdc/hca/HcaApplication.java
@@ -0,0 +1,13 @@
+package com.pdc.hca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdc/hca/HcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdc.hca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
